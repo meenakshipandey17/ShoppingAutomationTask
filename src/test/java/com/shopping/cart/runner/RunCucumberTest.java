d8f81dfaf291accdbcec656593ef17fbc6b43a0d
meenakshi <meenakshi.p.pandey@oracle.com>
2020-09-23 00:11:04 +0100

    modified runner to have capabilty to run the test using mvn test

--- src/test/java/com/shopping/cart/runner/RunCucumberTest.java
+++ src/test/java/com/shopping/cart/runner/RunCucumberTest.java
@@ -30,6 +30,16 @@ public class RunCucumberTest {
         testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
     }
 
+    @Test( description = "Runs Cucumber Feature Files", dataProvider = "features" )
+    public void feature(CucumberFeatureWrapper cucumberFeature) {
+        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+    }
+
+    @DataProvider
+    public Object[][] features() {
+        return testNGCucumberRunner.provideFeatures();
+    }
+
     @AfterClass( alwaysRun = true )
     public void tearDownClass() throws Exception {
         testNGCucumberRunner.finish();
